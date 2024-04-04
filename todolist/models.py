1bea6231fa1473c75d12c59d22c89505b43ea311
Muckilein <JuliaWessolleck@t-online.de>
2024-04-04 12:00:17 +0200

    requirements2

--- todolist/models.py
+++ todolist/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User # new
+from datetime import datetime  
+
+
+class TodoItem(models.Model):
+    title = models.CharField(max_length=100)
+    created_at= models.DateTimeField(default=datetime.now)
+    author= models.ForeignKey(User, on_delete=models.CASCADE)
+    checked = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return f'({self.id}) {self.title}'
 
 # Create your models here.
