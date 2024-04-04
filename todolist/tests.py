8dd394a876903076da1aefeaddb27df8232911e9
Muckilein <JuliaWessolleck@t-online.de>
2024-04-04 12:19:53 +0200

    test

--- todolist/tests.py
+++ todolist/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
-
+from .models import TodoItem
+from datetime import datetime 
+from django.contrib.auth.models import User
 # Create your tests here.
+# Create your tests here.
+class JoinTest(TestCase):   
+    
+    def test_makeTodo(self):       
+                
+        user = User.objects.create_user('test_user', password='test_user')
+        todo = TodoItem.objects.create(title = 'Schreiben',created_at=datetime.now,author=user,checked=True)        
+        # self.client.login(username='munni', password='111abcdefgh')       
+        # response = self.client.get('/todos/')            
+        # self.assertEqual(response.status_code, 200)
+        
